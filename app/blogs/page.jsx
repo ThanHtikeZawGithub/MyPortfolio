706cb03f428b61e6de237f5657085c19794e766d
ThanHtikeZaw <125514456+ThanHtikeZawGithub@users.noreply.github.com>
2023-08-28 13:25:24 +0630

    add work page

---
+++ app/blogs/page.jsx
@@ -0,0 +1,14 @@
+'use client'
+import React from 'react'
+import PageTransition from '../PageTransition'
+
+const Blogs = () => {
+  return (
+    <div>
+    <PageTransition />
+    Blogs
+    </div>
+  )
+}
+
+export default Blogs
---
+++ app/work/page.jsx
@@ -0,0 +1,14 @@
+'use client'
+import React from 'react'
+import PageTransition from '../PageTransition'
+
+const Work = () => {
+  return (
+    <div>
+    <PageTransition />
+        Work
+    </div>
+  )
+}
+
+export default Work
